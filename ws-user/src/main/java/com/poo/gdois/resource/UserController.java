43485ba7e9430b9eb54c465dc8b59e546543dd8e
Pauloasdf <paulopiresavila@gmail.com>
2021-11-22 22:21:42 -0300

    Alteração revertida

--- ws-user/src/main/java/com/poo/gdois/resource/UserController.java
+++ ws-user/src/main/java/com/poo/gdois/resource/UserController.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping(path = "/users")
 public class UserController extends AbstractController {
 
